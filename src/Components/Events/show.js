a66d28ad6517318572a9db975e0751b88ef5a69a
dan2811 <djordandrums@gmail.com>
2022-11-30 21:15:55 +0000

    add event show page with tabbed view and related jobs

--- src/Components/Events/show.js
+++ src/Components/Events/show.js
@@ -1,89 +1,87 @@
 import * as React from 'react';
 import {
   Show,
+  List,
   TextField,
-  SimpleShowLayout,
   EmailField,
   DateField,
   FunctionField,
   ChipField,
   SingleFieldList,
-  ReferenceArrayField,
+  ReferenceManyField,
   ArrayField,
   NumberField,
+  useRecordContext,
+  TabbedShowLayout,
+  Tab,
+  Datagrid,
+  BooleanField,
+  ReferenceArrayField,
+  ReferenceField,
+  ReferenceOneField,
+  WithRecord,
 } from 'react-admin';
+import { CustomReferenceManyField } from '../custom/CustomReferenceManyField.js';
+import MilitaryTechIcon from '@mui/icons-material/MilitaryTech';
 
-export const EventShow = () => (
-  <Show
-    title={
-      <FunctionField
-        render={(record) =>
-          `${record.client} ${record.package.data.attributes.name}`
-        }
-      />
-    }
-  >
-    <SimpleShowLayout>
-      <TextField label='Package' source='package.data.attributes.name' />
-      <TextField source='client' label='Client' />
-      <DateField source='date' />
-      <TextField source='location' />
-      <NumberField
-        label='Price'
-        source='gross'
-        options={{ style: 'currency', currency: 'GBP' }}
-      />
-      <NumberField
-        source='deposit'
-        options={{ style: 'currency', currency: 'GBP' }}
-      />
-      <NumberField
-        source='amountDue'
-        options={{ style: 'currency', currency: 'GBP' }}
-      />
-      <NumberField
-        source='profit'
-        options={{ style: 'currency', currency: 'GBP' }}
-      />
-
-      <ReferenceArrayField
-        label='Instruments Required'
-        source='instruments'
-        reference='instruments'
-      >
-        <ArrayField source='instruments'>
-          <SingleFieldList linkType='show'>
-            <ChipField source='name' />
-          </SingleFieldList>
-        </ArrayField>
-      </ReferenceArrayField>
-
-      <TextField source='notes' emptyText='None' />
-
-      <ReferenceArrayField
-        label='Musicians'
-        source='musicians'
-        reference='musicians'
-      >
-        <ArrayField source='musicians'>
-          <SingleFieldList linkType='show'>
-            <ChipField source='fName' />
-          </SingleFieldList>
-        </ArrayField>
-      </ReferenceArrayField>
-      <TextField source='team' />
-
-      {/* <TextField source='lName' label='Last Name' />
-      <EmailField source='email' label='Email' />
-      <DateField source='createdAt' label='Joined' />
-      <DateField source='updatedAt' label='Last edited' showTime={true} />
-      <ReferenceArrayField label='Pupils' source='pupils' reference='pupils'>
-        <ArrayField source='pupils'>
-          <SingleFieldList linkType='show'>
-            <ChipField source='fName' />
-          </SingleFieldList>
-        </ArrayField>
-      </ReferenceArrayField> */}
-    </SimpleShowLayout>
-  </Show>
-);
+export const EventShow = () => {
+  return (
+    <Show
+      title={
+        <FunctionField
+          render={(record) =>
+            record
+              ? `${record.client} ${record.package.data.attributes.name}`
+              : 'null'
+          }
+        />
+      }
+    >
+      <TabbedShowLayout>
+        <Tab label='Details'>
+          <TextField label='Package' source='package.data.attributes.name' />
+          <TextField source='client' label='Client' />
+          <DateField source='date' />
+          <TextField source='location' />
+          <TextField source='notes' emptyText='None' />
+        </Tab>
+        <Tab label='Finance'>
+          <NumberField
+            label='Price'
+            source='gross'
+            options={{ style: 'currency', currency: 'GBP' }}
+          />
+          <NumberField
+            source='deposit'
+            options={{ style: 'currency', currency: 'GBP' }}
+          />
+          <NumberField
+            source='amountDue'
+            options={{ style: 'currency', currency: 'GBP' }}
+          />
+          <NumberField
+            source='profit'
+            options={{ style: 'currency', currency: 'GBP' }}
+          />
+        </Tab>
+        <Tab label='Musicians'>
+          <CustomReferenceManyField reference='jobs' target='event.data.id'>
+            <Datagrid>
+              <BooleanField source='md' TrueIcon={MilitaryTechIcon} />
+              <FunctionField
+                label='Musician'
+                render={(record) =>
+                  `${record.musician.data.attributes.fName} ${record.musician.data.attributes.lName}`
+                }
+              />
+              <TextField
+                source='instrument.data.attributes.name'
+                label='Instrument'
+              />
+            </Datagrid>
+          </CustomReferenceManyField>
+        </Tab>
+      </TabbedShowLayout>
+    </Show>
+  );
+};
