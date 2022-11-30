5eb8bf2c5f765ea847ae8303af3ea87630d1e8a4
dan2811 <djordandrums@gmail.com>
2022-11-30 21:16:21 +0000

    add new list views to app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Admin, Resource } from 'react-admin';
 import { EventCreate } from './Components/Events/create';
 import { EventList } from './Components/Events/list';
 import { EventShow } from './Components/Events/show';
+import { MusicianList } from './Components/Musicians/list';
+import { JobList } from './Components/Jobs/list';
 import { DataProvider } from './DataProvider/index';
 
 const dataProvider = DataProvider('http://localhost:1337/api');
@@ -16,6 +18,8 @@ const App = () => (
       show={EventShow}
       create={EventCreate}
     />
+    <Resource name='musicians' list={MusicianList} />
+    <Resource name='jobs' list={JobList} />
   </Admin>
 );
 export default App;
