35ad4ea416f06df135c252624293664637dbabbe
dan2811 <djordandrums@gmail.com>
2022-12-04 01:27:05 +0000

    list jobs

--- src/Components/Jobs/list.js
+++ src/Components/Jobs/list.js
@@ -3,31 +3,35 @@ import {
   List,
   Datagrid,
   TextField,
-  ArrayField,
-  SingleFieldList,
-  ChipField,
-  EditButton,
-  ReferenceArrayField,
   DateField,
   NumberField,
   FunctionField,
+  BooleanField,
 } from 'react-admin';
 
 export const JobList = () => {
   return (
     <List>
       <Datagrid>
-        <TextField source='md' label='MD' />
-        <TextField
-          source='instrument.data.attributes.name'
-          label='Instrument'
-        />
+        <DateField source='event.data.attributes.date' label='Date' />
         <FunctionField
           label='Musician'
           render={(record) =>
             `${record.musician.data.attributes.fName} ${record.musician.data.attributes.lName}`
           }
         />
+        <TextField source='md' label='MD' />
+        <TextField
+          source='instrument.data.attributes.name'
+          label='Instrument'
+        />
+        <NumberField
+          label='Wage'
+          source='wage'
+          options={{ style: 'currency', currency: 'GBP' }}
+          emptyText={'Not agreed'}
+        />
+        <BooleanField source='hotelRequired' />
       </Datagrid>
     </List>
   );
