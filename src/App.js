f691f2a6ebd95e63dbd263e368261b5858fe6889
dan2811 <djordandrums@gmail.com>
2022-12-04 01:28:18 +0000

    implement new components

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ import { EventShow } from './Components/Events/show';
 import { MusicianList } from './Components/Musicians/list';
 import { JobList } from './Components/Jobs/list';
 import { DataProvider } from './DataProvider/index';
+import { SetShow } from './Components/Sets/show.js';
+import { MusicianCreate } from './Components/Musicians/create.js';
+import { MusicianEdit } from './Components/Musicians/edit.js';
+import { InstrumentsList } from './Components/Instruments/list';
+import { InstrumentCreate } from './Components/Instruments/create';
+import { MusicianShow } from './Components/Musicians/show';
+import { InstrumentEdit } from './Components/Instruments/edit';
 
 const dataProvider = DataProvider('http://localhost:1337/api');
 
@@ -18,8 +25,23 @@ const App = () => (
       show={EventShow}
       create={EventCreate}
     />
-    <Resource name='musicians' list={MusicianList} />
+    <Resource
+      name='musicians'
+      list={MusicianList}
+      create={MusicianCreate}
+      edit={MusicianEdit}
+      show={MusicianShow}
+    />
     <Resource name='jobs' list={JobList} />
+    <Resource name='sets' show={SetShow} />
+    <Resource name='songs' />
+    <Resource
+      name='instruments'
+      list={InstrumentsList}
+      create={InstrumentCreate}
+      edit={InstrumentEdit}
+      recordRepresentation='name'
+    />
   </Admin>
 );
 export default App;
