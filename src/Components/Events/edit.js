e6d40d695e22603bee757d46e1d9895e34a3bb90
dan2811 <djordandrums@gmail.com>
2022-12-06 16:48:09 +0000

    event edit page

---
+++ src/Components/Events/edit.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { SimpleForm, required, Edit, DateInput, TextInput } from 'react-admin';
+
+export const EventEdit = () => {
+  return (
+    <Edit>
+      <SimpleForm>
+        <DateInput source='date' validate={[required()]} />
+        <TextInput source='client' validate={[required()]} />
+        <TextInput source='location' />
+        <TextInput source='notes' />
+        
+
+      </SimpleForm>
+    </Edit>
+  );
+};
