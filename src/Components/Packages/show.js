7f092018edfab95557e40e96047b4b90cb9937a4
dan2811 <djordandrums@gmail.com>
2022-12-09 17:01:16 +0000

    add filtered event list to package show page

--- src/Components/Packages/show.js
+++ src/Components/Packages/show.js
@@ -12,40 +12,48 @@ import {
   Datagrid,
   BooleanField,
   useRecordContext,
+  useLoading,
 } from 'react-admin';
 import { Card } from '@mui/material';
 import CustomReferenceManyField from '../custom/CustomReferenceManyField';
 
-
 const Title = () => {
   const record = useRecordContext();
   if (!record) return null;
-  return (
-    <span>
-      {record.name}
-    </span>
-  );
+  return <span>{record.name}</span>;
 };
+
+const FilteredEventList = () => {
+  const record = useRecordContext();
+  console.log("YOOOO", record);
+  if (!record) return null;
+  return (
+    record.events.length === 0 ? (
+    <div>None</div>
+  ) : (
+    <Card>
+      <CustomReferenceManyField
+        reference='events'
+        target='package.data.id'
+        resource='events'
+      >
+        <Datagrid rowClick={'show'}>
+          <DateField source='date' />
+          <TextField source='client' />
+          <TextField source='team' />
+        </Datagrid>
+      </CustomReferenceManyField>
+    </Card>
+  ));
+};
+
 
 export const PackageShow = () => {
   return (
     <Show title={<Title />}>
       <TabbedShowLayout>
         <Tab label='Events'>
-          <Card>
-            <CustomReferenceManyField
-              reference='events'
-              target='package.data.id'
-              resource='events'
-            >
-              <Datagrid rowClick={'show'}>
-                <DateField source='date'/>
-                <TextField source='client'/>
-                <TextField source='team' />
-
-              </Datagrid>
-            </CustomReferenceManyField>
-          </Card>
+          <FilteredEventList />
         </Tab>
       </TabbedShowLayout>
     </Show>
