39c4fd71b3dfe32eb7ad12546ff1fa4dc1efbeb1
dan2811 <djordandrums@gmail.com>
2023-01-09 20:19:24 +0000

    change default port to 80

--- scripts/heroku-start.js
+++ scripts/heroku-start.js
@@ -2,7 +2,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
+
 app.use(express.json());
 // Your static pre-build assets folder
 app.use(express.static(path.join(__dirname, '..', 'build')));
