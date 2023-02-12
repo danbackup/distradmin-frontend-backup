109112a7a5f865bbb31857b0a075f44e4e84abab
dan2811 <djordandrums@gmail.com>
2023-02-12 19:17:43 +0000

    small refactor to google doc button

--- src/Components/Events/customEventComponents/googleDocButton.js
+++ src/Components/Events/customEventComponents/googleDocButton.js
@@ -1,36 +1,32 @@
 import React from 'react';
-import { Button } from 'react-admin';
+import Button from '@mui/material/Tooltip';
 import CircularProgress from '@mui/material/CircularProgress';
 
 const GoogleDocButton = ({ isLoading, googleDocId, open, create, record }) => {
   console.log('Google Doc ID: ' + googleDocId);
   if (!record) return null;
   if (isLoading) return <CircularProgress />;
-  if (!googleDocId) {
-    return (
-      <Button
-        label='Create Document'
-        size='small'
-        alignIcon='left'
-        variant='contained'
-        color='secondary'
-        sx={{ width: 200 }}
-        onClick={() => create(record)}
-      />
-    );
-  } else {
-    return (
-      <Button
-        label='Open Document'
-        size='small'
-        alignIcon='left'
-        variant='contained'
-        color='secondary'
-        sx={{ width: 200 }}
-        onClick={() => open(googleDocId)}
-      />
-    );
-  }
+  return !googleDocId ? (
+    <Button
+      label='Create Document'
+      size='small'
+      alignIcon='left'
+      variant='contained'
+      color='secondary'
+      sx={{ width: 200 }}
+      onClick={() => create(record)}
+    />
+  ) : (
+    <Button
+      label='Open Document'
+      size='small'
+      alignIcon='left'
+      variant='contained'
+      color='secondary'
+      sx={{ width: 200 }}
+      onClick={() => open(googleDocId)}
+    />
+  );
 };
 
 export default GoogleDocButton;
