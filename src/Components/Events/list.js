5ef03de4ce415afda68a07b292ae186ef03e23ed
dan2811 <djordandrums@gmail.com>
2023-02-15 17:17:22 +0000

    implement filters on event list

--- src/Components/Events/list.js
+++ src/Components/Events/list.js
@@ -8,16 +8,43 @@ import {
   FunctionField,
   ReferenceField,
   usePermissions,
+  ReferenceArrayInput,
+  AutocompleteArrayInput,
+  DateInput,
+  TextInput,
+  NumberInput,
 } from 'react-admin';
 import { CircularProgressWithLabel } from '../custom/circularProgress';
 import ColouredDateField from './customEventComponents/colouredDateField';
 
 export const EventList = () => {
+  const standardFilters = [
+    <TextInput label='Client Name' source='client_containsi' alwaysOn />,
+    <TextInput label='Location' source='location_containsi' />,
+    <TextInput label='Team' source='team_containsi' />,
+    <ReferenceArrayInput
+      label='Package'
+      source='package.id'
+      reference='packages'
+    >
+      <AutocompleteArrayInput label='Package' />
+    </ReferenceArrayInput>,
+    <DateInput label='Date from' source='date_gte' />,
+  ];
+  const superAdminFilters = [
+    ...standardFilters,
+    <NumberInput label='Profit' source='profit_gte' />,
+  ];
+
   const { isLoading, permissions } = usePermissions();
+
+  const filters =
+    permissions === 'Super Admin' ? superAdminFilters : standardFilters;
+
   return isLoading ? (
     <div>Checking permissions...</div>
   ) : (
-    <List sort={{ field: 'date', order: 'DESC' }}>
+    <List filters={filters} sort={{ field: 'date', order: 'DESC' }}>
       <Datagrid rowClick='show'>
         <ColouredDateField source='date' />
         <TextField source='type' />
